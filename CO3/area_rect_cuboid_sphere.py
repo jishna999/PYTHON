7d23142681b824b397e13f7abf165823a77890e9
jishna999 <95485984+jishna999@users.noreply.github.com>
2022-02-28 18:21:50 +0530

    Add files via upload

---
+++ CO3/area_rect_cuboid_sphere.py
@@ -0,0 +1,10 @@
+
+from graphics import rectangle as r
+from graphics import circle as c
+from graphics.threedgraphics import cuboid as cu
+from graphics.threedgraphics import sphere as s
+
+print(c.area(2))
+print(c.peri(2))
+print(r.area(2, 2))
+print(r.perim(2, 2))
